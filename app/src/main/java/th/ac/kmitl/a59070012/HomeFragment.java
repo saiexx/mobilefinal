4c133a693d31d988ef9bc573fcba6018b2ac22c7
LAB203_33 <59070012@kmitl.ac.th>
2018-12-11 12:53:03 +0700

    add dbhelper

---
+++ app/src/main/java/th/ac/kmitl/a59070012/HomeFragment.java
@@ -0,0 +1,21 @@
+package th.ac.kmitl.a59070012;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HomeFragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home,container,false);
+    }
+}
--- app/src/main/java/th/ac/kmitl/a59070012/LoginFragment.java
+++ app/src/main/java/th/ac/kmitl/a59070012/LoginFragment.java
@@ -1,16 +1,19 @@
 package th.ac.kmitl.a59070012;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class LoginFragment extends android.support.v4.app.Fragment {
+
+    DatabaseHelper databaseHelper = new DatabaseHelper(getActivity());
 
     @Nullable
     @Override
@@ -22,6 +25,7 @@ public class LoginFragment extends android.support.v4.app.Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         initRegisterButton();
+        initLoginButton();
     }
 
     public void initRegisterButton(){
@@ -32,5 +36,20 @@ public class LoginFragment extends android.support.v4.app.Fragment {
                 getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.main_view, new RegisterFragment()).addToBackStack(null).commit();
             }
         });
+    }
+
+    public void initLoginButton(){
+        Button loginButton = getView().findViewById(R.id.login_login_btn);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText username = getView().findViewById(R.id.login_user_id);
+                EditText password = getView().findViewById(R.id.login_password);
+                if(databaseHelper.checkUser(username.getText().toString().trim() , password.getText().toString().trim())){
+                    Toast.makeText(getActivity(),"success",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
     }
 }
